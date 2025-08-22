6abcaf2922702904de1ddfd201b48ab231be7fd3
Vladimir Pyshkin <vladimirpyshkin2007@gmail.com>
2025-08-22 19:46:24 +0300

    Update SquareSolver.cpp
    add function for comparing double with null, change SolveEquation, square_equation

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -12,13 +12,20 @@ enum cntRoots
 	TWO = 2
 };
 
+enum CompWithNull
+{
+	ABOVE = 1,
+	EQUAL = 0,
+	BELLOW = -1
+};
+
 const float EPS = 1e-10f;
 
 cntRoots SolveEquation(double a, double b, double c, double *x1, double *x2);
 cntRoots linear_equation(double *x1, double b, double c);
 cntRoots square_equation(double *x1, double *x2, double a, double b, double c);
 
-bool is_zero(double x);
+CompWithNull CompareDoubleNull(double n);
 
 void NameOfProgrammAndAuthor(void);
 
@@ -34,8 +41,8 @@ int main(void) {
 
 	printf("Enter coefficients of square equation: ");
 
-	double coef_a = NAN, coef_b = 0, coef_c = 0;		// add NAN
-	double x1 = 0, x2 = 0;
+	double coef_a = NAN, coef_b = NAN, coef_c = NAN;
+	double x1 = NAN, x2 = NAN;
 	Input(&coef_a, &coef_b, &coef_c);
 	cntRoots nRoots = SolveEquation(coef_a, coef_b, coef_c, &x1, &x2);
 	OutputRoots(nRoots, x1, x2);
@@ -46,14 +53,18 @@ int main(void) {
 
 
 cntRoots SolveEquation(double a, double b, double c, double *x1, double *x2) {
-	if (is_zero(a) && is_zero(b) && is_zero(c)) {
-		return INF;
-	}
-	if (is_zero(a) && is_zero(b) && !is_zero(c)) {
-		return ZERO;
-	}
-	if (is_zero(a)) {
-		return linear_equation(x1, b, c);
+	if (CompWithNull(a) == EQUAL) {
+		if (CompWithNull(b) == EQUAL) {
+			if (CompWithNull(c) == EQUAL) {
+				return INF;
+			}
+			else {
+				return ZERO;
+			}
+		}
+		else {
+			return linear_equation(x1, b, c);
+		}
 	}
 	else {
 		return square_equation(x1, x2, a, b, c);
@@ -70,23 +81,23 @@ cntRoots linear_equation(double *x1, double b, double c) {
 
 
 cntRoots square_equation(double *x1, double *x2, double a, double b, double c) {
-	if (is_zero(c)) {
+	if (CompWithNull(c) == EQUAL) {
 		*x1 = 0;
-		*x2 = - (b / a);	// call linear_equation		ax+b=0
+		linear_equation(x2, a, b);	// call linear_equation		ax+b=0
 
 		return TWO;
 	}
 	else {
 		double disc = b * b - 4 * a * c;
 
-		if (is_zero(disc)) {
+		if (CompWithNull(disc) == EQUAL) {
 			*x1 = - (b / (2 * a));
 
 			return ONE;
 		}
 		else if (disc > 0) {			// add func for compare double and null
-			*x1 = (-b + sqrt(disc)) / (2*a);
-			*x1 = (-b - sqrt(disc)) / (2*a);
+			*x1 = (-b + sqrt(disc)) / (2 * a);
+			*x2 = (-b - sqrt(disc)) / (2 * a);
 
 			return TWO;
 		}
@@ -96,16 +107,24 @@ cntRoots square_equation(double *x1, double *x2, double a, double b, double c) {
 	}
 }
 
+CompWithNull CompareDoubleNull(double n) {
+	if (n > EPS) {
+		return ABOVE;
+	}
+	else if (abs(n) < EPS) {
+		return EQUAL;
+	}
+	else {
+		return BELLOW;
+	}
+
+}
 
 void NameOfProgrammAndAuthor(void) {
 	printf("# Square equation solver\n"
         "# (c) NeonVP, 2025\n\n");
 }
 
-
-bool is_zero(double x) {
-	return (abs(x) < EPS);
-}
 
 // stdin 
 // stdout
