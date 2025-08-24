a76421b185cb26c07927eea4ff327bb3b93098d2
Vladimir Pyshkin <vladimirpyshkin2007@gmail.com>
2025-08-24 10:46:26 +0300

    add colors

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -5,11 +5,14 @@
 // read about dynamic memory and calloc, realloc, malloc
 enum cntRoots
 {
-	INF = -1,
+	INF = 3,
 	ZERO = 0, 
 	ONE = 1, 
-	TWO = 2
+	TWO = 2,
+	UnknownErr = 999
 };
+
+const char* ListCntRoots[4] = {"ZERO", "ONE", "TWO", "INF"};
 
 enum CompWithNull
 {
@@ -18,60 +21,62 @@ enum CompWithNull
 	BELLOW = -1
 };
 
+/*-------COLORS-------*/
+#define COLOR_RED		"\x1b[31m"
+#define COLOR_GREEN		"\x1b[32m"
+#define COLOR_YELLOW	"\x1b[33m"
+#define COLOR_BLUE		"\x1b[34m"
+#define COLOR_CYAN		"\x1b[36m"
+#define COLOR_RESET		"\x1b[0m"
+
 const float EPS = 1e-10f;
 
 cntRoots SolveEquation(double *coefs, double *roots);
 cntRoots linear_equation(double *x1, double b, double c);
-cntRoots square_equation(double *roots, double *coefs);
+cntRoots square_equation(double *coefs, double *roots);
 
 CompWithNull CompareDoubleNull(double n);
 
 void NameOfProgrammAndAuthor();
 
-int Input(double *coefs);
+void Input(double *coefs);
 int OutputRoots(cntRoots nRoots, double *roots);
 
-bool ClearBuffer();
+void ClearBuffer();
 
 void TestSolveEquation();
-const char* cntRoots_to_str(cntRoots string);
 
 int main() {
 	NameOfProgrammAndAuthor();
 
 	printf("Enter coefficients of square equation: ");
-
 	double coefs[3] = {};
 	double roots[2] = {};
 	Input(coefs);
 	cntRoots nRoots = SolveEquation(coefs, roots);
 	OutputRoots(nRoots, roots);
-
 	return 0;
 }
 
+// int main() {
+// 	TestSolveEquation();
+//
+// 	return 0;
+// }
+
 
 cntRoots SolveEquation(double *coefs, double *roots) {
+	assert(coefs != NULL);
+	assert(roots != NULL);
 	assert(isfinite (coefs[0]));
 	assert(isfinite (coefs[1]));
 	assert(isfinite (coefs[2]));
-	assert(roots != NULL);
 
-	if (CompWithNull(coefs[0]) == EQUAL) {
-		if (CompWithNull(coefs[1]) == EQUAL) {
-			if (CompWithNull(coefs[2]) == EQUAL) {
-				return INF;
-			}
-			else {
-				return ZERO;
-			}
-		}
-		else {
-			return linear_equation(&(roots[0]), coefs[1], coefs[2]);
-		}
+	if (CompareDoubleNull(coefs[0]) == EQUAL) {
+		return linear_equation(&(roots[0]), coefs[1], coefs[2]);
 	}
 	else {
-		return square_equation(roots, coefs);
+		return square_equation(coefs, roots);
 	}
 }
 
@@ -81,20 +86,31 @@ cntRoots linear_equation(double *x1, double b, double c) {
 	assert(isfinite (c));
 	assert(x1 != NULL);
 
-	printf("This is not a square equation, but a linear one.\n");
-	*x1 = - (c / b);
+	if (CompareDoubleNull(b) == EQUAL) {
+		if (CompareDoubleNull(c) == EQUAL) {
+			return INF;
+		}
+		else {
+			return ZERO;
+		}
+	}
+	else {
+		printf("This is not a square equation, but a linear one.\n");
+		*x1 = - (c / b);
 
-	return ONE;
+		return ONE;
+	}
 }
 
 
-cntRoots square_equation(double *roots, double *coefs) {
+cntRoots square_equation(double *coefs, double *roots) {
+	assert(coefs != NULL);
+	assert(roots != NULL);
 	assert(isfinite (coefs[0]));
 	assert(isfinite (coefs[1]));
 	assert(isfinite (coefs[2]));
-	assert(roots != NULL);
 
-	if (CompWithNull(coefs[2]) == EQUAL) {
+	if (CompareDoubleNull(coefs[2]) == EQUAL) {
 		roots[0] = 0;
 		linear_equation(&(roots[1]), coefs[0], coefs[1]);	// call linear_equation		ax+b=0
 
@@ -103,24 +119,24 @@ cntRoots square_equation(double *roots, double *coefs) {
 	else {
 		double disc = coefs[1] * coefs[1] - 4 * coefs[0] * coefs[2];
 
-		if (CompWithNull(disc) == EQUAL) {
-			roots[0] = - (coefs[1] / (2 * coefs[0]));
-
-			return ONE;
-		}
-		else if (disc > 0) {			// add func for compare double and null
-			roots[0] = (-coefs[1] - sqrt(disc)) / (2 * coefs[0]);
-			roots[1] = (-coefs[1] + sqrt(disc)) / (2 * coefs[0]);
-
-			return TWO;
-		}
-		else {
-			return ZERO;
+		switch (CompareDoubleNull(disc)) {
+			case EQUAL:
+				roots[0] = - (coefs[1] / (2 * coefs[0]));
+				return ONE;
+			case ABOVE:
+				roots[0] = (-coefs[1] - sqrt(disc)) / (2 * coefs[0]);
+				roots[1] = (-coefs[1] + sqrt(disc)) / (2 * coefs[0]);
+				return TWO;
+			case BELLOW:
+				return ZERO;
+			default:
+				return UnknownErr;
 		}
 	}
 }
 
-CompWithNull CompareDoubleNull(double n) {
+
+CompWithNull CompareDoubleNull(double n) {  //
 	if (n > EPS) {
 		return ABOVE;
 	}
@@ -134,53 +150,44 @@ CompWithNull CompareDoubleNull(double n) {
 }
 
 void NameOfProgrammAndAuthor() {
-	printf("# Square equation solver\n"
-        "# (c) NeonVP, 2025\n\n");
+	printf(	COLOR_CYAN
+			"##############################\n"
+			"#   Square equation solver   #\n"
+			"#         (c) NeonVP, 2025   #\n"
+			"##############################\n\n"
+			COLOR_RESET );
 }
 
-
-// stdin 
-// stdout
-// stderr
-
-//read about eof
-
-bool ClearBuffer() {
+void ClearBuffer() {
 	while (getchar() != EOF) {}
-	return false;
 }
 
-int Input(double *coefs) {
+void Input(double *coefs) {
 	while (scanf("%lg %lg %lg", &coefs[0], &coefs[1], &coefs[2]) != 3) {
 		ClearBuffer();
 		printf("Incorrect Input. Try again, please.\n"
 		  "Enter coefficients of square equation: ");
 	}
-	return 0;
 } 
 
 
-int OutputRoots(cntRoots nRoots, double *roots) {
+void OutputRoots(cntRoots nRoots, double *roots) {  //
 	switch (nRoots){
 		case INF:
 			printf("The equation has an infinity number of roots.\n");
-			return 1;
 		case ZERO:
 			printf("There are no roots.\n");
-			return 1;
 		case ONE:
 			printf("There is 1 roots: %lg.\n", roots[0]);
-			return 1;
 		case TWO:
 			printf("There is 2 roots: %lg and %lg.\n", roots[0], roots[1]);
-			return 1;
 		default:
-			printf("Unknown number of solutions");
+			printf("!!! Error !!!\n Unknown number of solutions. \n");
 	}
-	return 0;
 }
 
 
+/* TODO: colors for tests, debug unitests, read about structures */
 void TestSolveEquation() {
 	double tests[8][2][3] =
 		{
@@ -196,26 +203,23 @@ void TestSolveEquation() {
 
 	cntRoots tests_nRoots[8] = {TWO, ONE, ZERO, ONE, TWO, TWO, INF, ZERO};
 
-	int i = 0;
-	for (i = 0; i < 7; i++) {
+	for (int i = 0; i < 8; i++) {
 		double roots[2] = {};
-		cntRoots nRoots = SolveEquation(tests[i][0], roots);		// 2, 3
-		if (!(nRoots == tests_nRoots[i] && roots[0] == tests[i][1][0] && roots[1] == tests[i][1][1])) {
-			printf("FAILED: SolveEquation(%lf, %lf, %lf, ...) -> %s, x1 = %lf, x2 = %lf (should be x1 = %lf, x2 = %lf, %s)\n",
-				tests[i][0][0], tests[i][0][1], tests[i][0][2],
-				cntRoots_to_str(nRoots),
-				roots[0], roots[1],
-				tests[i][1][0], tests[i][1][1], tests_nRoots[i]);
-		}
-	}
-}
+		cntRoots nRoots = SolveEquation(tests[i][0], roots);
 
-const char* cntRoots_to_str(cntRoots string) {
-	switch (string) {
-		case INF: return "INF";
-		case ZERO: return "0";
-		case ONE: return "1";
-		case TWO: return "2";
-		default: return "Error";
+		if (!(nRoots == tests_nRoots[i] && roots[0] == tests[i][1][0] && roots[1] == tests[i][1][1])) {
+			printf(COLOR_RED "FAILED: SolveEquation(%lf, %lf, %lf, ...) -> %s, x1 = %lf, x2 = %lf (should be x1 = %lf, x2 = %lf, %s)\n" COLOR_RESET,
+				   tests[i][0][0], tests[i][0][1], tests[i][0][2],
+				   ListCntRoots[nRoots],
+				   roots[0], roots[1],
+				   tests[i][1][0], tests[i][1][1], ListCntRoots[tests_nRoots[i]]);
+		}
+		else {
+			printf(COLOR_GREEN "OK: SolveEquation(%lf, %lf, %lf, ...) -> %s, x1 = %lf, x2 = %lf (should be x1 = %lf, x2 = %lf, %s)\n" COLOR_RESET,
+				   tests[i][0][0], tests[i][0][1], tests[i][0][2],
+				   ListCntRoots[nRoots],
+				   roots[0], roots[1],
+				   tests[i][1][0], tests[i][1][1], ListCntRoots[tests_nRoots[i]]);
+		}
 	}
 }
